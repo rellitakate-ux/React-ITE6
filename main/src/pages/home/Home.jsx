82d286598e9ab69764285a0ae5b80ed3c8f4a31e
Kate <rellitakate@gmail.com>
2026-03-31 10:34:18 +0800

    2nd commit

---
+++ main/src/pages/home/Home.jsx
@@ -0,0 +1,28 @@
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <main className="home">
+      <h1>BSIT-2A Group 3 - Members</h1>
+      <p>Our group is consist of 4 dedicated members.</p>
+      <section className="members">
+        <ul>
+          <li>
+            <strong>Lee Conarco</strong>
+          </li>
+          <li>
+            <strong>Dailyn Collamar</strong>
+          </li>
+          <li>
+            <strong>Jerson Manlabe</strong>
+          </li>
+          <li>
+            <strong>Katelyn Relllita</strong>
+          </li>
+        </ul>
+      </section>
+    </main>
+  );
+};
+
+export default Home;
